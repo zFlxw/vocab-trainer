fd15de536d69009e5a6b6ac95fc1f3537654ebf9
zFlxw <lxcalflxw@gmail.com>
2022-03-21 21:56:55 +0100

    Initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <HomeView />
+  <router-view />
+  <footer>
+    <a href="https://github.com/zFlxw" target="_blank">&copy; 2022 by Flxw</a>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HomeView from './views/HomeView.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: { HomeView }
+})
+</script>
+
+<style lang="scss">
+$background: #f6f8fa;
+$main_blue: #0077b6;
+$main_grey: #495057;
+$second_grey: #ced4da;
+
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+main {
+  min-height: calc(97vh - 3px);
+  display: grid;
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-family: 'Poppins', sans-serif;
+}
+
+footer {
+  width: 100%;
+  height: 3vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.25rem;
+  color: $second_grey;
+  text-align: center;
+  transition: .15s;
+
+  a {
+    text-decoration: none;
+    color: second_grey;
+
+    &:visited, &:active {
+      color: inherit;
+    }
+  }
+
+  &:hover {
+    cursor: pointer;
+    color: $main_grey;
+  }
+}
+</style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,98 @@
+<template>
+  <main>
+    <div class="centered">
+      <div class="title">
+        <h1>Vokabeltrainer<p>.</p></h1>
+      </div>
+      <div class="childs">
+        <h2 class="link-anim">Lernen<p>.</p></h2> <h2 class="link-anim">Erstellen<p>.</p></h2>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'HomeView'
+}
+</script>
+
+<style lang="scss">
+$background: #f6f8fa;
+$main_blue: #0077b6;
+$main_grey: #495057;
+$second_grey: #ced4da;
+
+*, *::before, *::after {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 18px;
+  height: 85vh;
+}
+
+body {
+  background-color: $background;
+
+  h1 p, h2 p {
+    color: $main_blue;
+    display: inline-flex;
+  }
+}
+
+.centered {
+  height: 75%;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 2rem;
+  color: $main_grey;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .title {
+    display: inline-block;
+    text-decoration: none;
+  }
+}
+
+.childs {
+  padding-top: 12.5rem;
+  font-size: 1.75rem;
+  width: 60rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+}
+
+.link-anim {
+  display: inline-block;
+  text-decoration: none;
+  cursor: pointer;
+  transition: .15s;
+
+  &::after {
+    content: '';
+    display: block;
+    width: 0;
+    height: 2px;
+    background: $main_blue;
+    transition: width .3s;
+  }
+
+  &:hover {
+    opacity: .9;
+  }
+
+  &:hover::after {
+    width: 100%;
+  }
+}
+</style>
